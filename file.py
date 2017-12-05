54c41cd183352ffaa7c3b30e6e13035f42413742
Jordan Rejaud <jrejaud@gmail.com>
2017-12-04 17:46:15 -0800

    Google and Here Geocoder working

--- file.py
+++ file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from abc import ABCMeta, abstractmethod
+import requests, json
 
 class Geocoder(object):
     """Abstract Geocoder class.
@@ -11,18 +12,61 @@ class Geocoder(object):
     def geocodeLocation(self, location):
         pass
 
+    # Validate that the address is correct
+    def validateAddress(self, location):
+        # TODO Implement this function
+        return True
 
 class GoogleGeocoder(Geocoder):
+
+    # TODO Move this key to a more secure location (system envs) as opposed to just keeping it in source
+    API_KEY = "AIzaSyBnM9ABU-ahZz-5_U0rEkeUCx6pD7lQ-zU";
+    BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json"
+
     def geocodeLocation(self, location):
-        print "Get Location from Google!";
+        if not self.validateAddress(location):
+            #return "Invalid address"
+            return False
+        print "Geting Location from Google's API"
+        payload = {"address": location, "key": self.API_KEY}
+        # This is synchronous, it will not scale once many users are hitting the server at once.
+        r = requests.get(self.BASE_URL, payload)
+        # Check if response is valid
+        if not r.status_code == 200:
+            #return "Invalid response"
+            return False
+
+        # TODO Check to make sure the json response is in this format
+        latLng = r.json()["results"][0]["geometry"]["location"]
+        print "Got latLng from Google ", latLng
+
+        # Construct a response
+        return json.dumps({"latitude":latLng["lat"], "longitude":latLng["lng"]})
 
 
-# Implement this later
-class SecondGeocoder(Geocoder):
+class HereGeocoder(Geocoder):
+
+    # TODO Move this key to a more secure location (system envs) as opposed to just keeping it in source
+    APP_ID = "SBeLfuAJ24jrljc0zYER"
+    APP_CODE = "VZpVZDanVRLoVNIHj4PObA"
+    BASE_URL = "https://geocoder.cit.api.here.com/6.2/geocode.json"
+
     def geocodeLocation(self, location):
-        print "Get Location from a second API";
+        if not self.validateAddress(location):
+            return "Invalid address"
+            return False
+        print "Getting Location from Geocoder API"
+        payload = {"app_id": self.APP_ID, "app_code": self.APP_CODE, "searchtext": location}
+        # This is synchronous, it will not scale once many users are hitting the server at once.
+        r = requests.get(self.BASE_URL, payload)
+        # Check if response is valid
+        if not r.status_code == 200:
+            return "Invalid response"
+            return False
 
+        # TODO Check to make sure the json response is in this format
+        latLng = r.json()["Response"]["View"][0]["Result"][0]["Location"]["DisplayPosition"]
+        print "Got latLng from Geocoder ", latLng
 
-
-x = GoogleGeocoder();
-x.geocodeLocation("sa");
+        # Construct a response
+        return json.dumps({"latitude":latLng["Latitude"], "longitude":latLng["Longitude"]})
--- server.py
+++ server.py
@@ -1,11 +1,39 @@
-#HTTP Server
+#!/usr/bin/env python
+# Server
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import urlparse
+
+from file import GoogleGeocoder, HereGeocoder
 
 class ServiceHandler(BaseHTTPRequestHandler):
     # Make this the only API endpoint, read the location that the user is passing
-    # TODO: How should location be broken up?
+    # localhost:9000?location=1234 fake street
     def do_GET(self):
-        print "Got HTTP Message!"
+        print "Incoming Get Call"
+        location = urlparse.parse_qs(urlparse.urlparse(self.path).query).get('location', None)
+        if not location:
+            print "User did not provide a location in GET call"
+            self.send_response(400)
+            self.end_headers()
+            return
+
+        # Try to get latLng from Google API
+        # response = googleGeocoder.geocodeLocation(location)
+        response = hereGeocoder.geocodeLocation(location)
+
+        # If this latLng has errors
+        if not response:
+            #Hit the other location API
+            print("No response")
+            self.send_response(500)
+            self.end_headers()
+            return
+
+
+        self.send_response(200)
+        self.send_header('Content-type','application/json')
+        self.end_headers()
+        self.wfile.write(response)
         return;
 
 def run(handler_class=ServiceHandler, port=9000):
@@ -15,5 +43,8 @@ def run(handler_class=ServiceHandler, port=9000):
 
     server.serve_forever();
 
+# Create a list of geocoders and iterate through them in order
+googleGeocoder = GoogleGeocoder();
+hereGeocoder = HereGeocoder();
 
 run();
